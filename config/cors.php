56955d5b93d936878ba40cb44d44e7b71708f70f
Alec col <a.colq.c@hotmail.com>
2026-03-05 18:43:44 -0400

    added cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5001', 'http://127.0.0.1:5001'],
+    'allowed_origins'          => ['https://encomiendas.culking.com', 'http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5001', 'http://127.0.0.1:5001'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
